41c36e6e968ab6289957494cca3cd5bcd6b8ebc1
Subash Bhattachan <bash_shoe@hotmail.com>
2024-11-28 00:00:06 -0500

    second commit

--- routes/grades_agg.mjs
+++ routes/grades_agg.mjs
@@ -63,7 +63,7 @@ router.get("/stats", async (req, res) => {
       {
         $project: {
           _id: 0,
-          student_id: "$_id",
+          // learner_id: "$_id",
           avg: {
             $sum: [
               { $multiply: [{ $avg: "$exam" }, 0.5] },
@@ -94,6 +94,82 @@ router.get("/stats", async (req, res) => {
   // I used $facet then 2 $project stages after the replicatation of the previous aggregation
 
 
+  router.get("/stats/:id", async (req, res) => {
+
+    const { id } = req.params; // to extract class_id 
+
+    let collection = await db.collection("grades");
+  
+    let result = await collection
+      .aggregate([
+        {
+          $match: { class_id: id } 
+        },
+        {
+          $unwind: { path: "$scores" },
+        },
+        {
+          $group: {
+            _id: "$learner_id",
+            quiz: {
+              $push: {
+                $cond: {
+                  if: { $eq: ["$scores.type", "quiz"] },
+                  then: "$scores.score",
+                  else: "$$REMOVE",
+                },
+              },
+            },
+            exam: {
+              $push: {
+                $cond: {
+                  if: { $eq: ["$scores.type", "exam"] },
+                  then: "$scores.score",
+                  else: "$$REMOVE",
+                },
+              },
+            },
+            homework: {
+              $push: {
+                $cond: {
+                  if: { $eq: ["$scores.type", "homework"] },
+                  then: "$scores.score",
+                  else: "$$REMOVE",
+                },
+              },
+            },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            student_id: "$_id",
+            avg: {
+              $sum: [
+                { $multiply: [{ $avg: "$exam" }, 0.5] },
+                { $multiply: [{ $avg: "$quiz" }, 0.3] },
+                { $multiply: [{ $avg: "$homework" }, 0.2] },
+              ],
+            },
+          },
+        },
+        {
+          $match: {
+            avg: { $gte: 70 },
+          }, 
+        },
+      ])
+      .toArray();
+      console.log(result);
+      const totalLearners = (await collection.distinct("learner_id", { class_id: id })).length;
+      const learnersWithOver70 = result.length;
+      const percentageover70 = ((learnersWithOver70 / totalLearners) * 100).toFixed(2);
+    
+  
+      if (!result) res.send("Not found").status(404);
+      else res.send({learnersWithOver70, totalLearners, percentageover70}).status(200);
+  
+  });
 
 
 
